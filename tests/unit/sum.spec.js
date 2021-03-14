36f7e83a32375f207822bfe16acbc79cb9677775
angel <angel.ir.arevalo@gmail.com>
2021-03-13 23:45:38 -0500

    Added tests for new Sum.vue

---
+++ tests/unit/sum.spec.js
@@ -0,0 +1,15 @@
+import { shallowMount } from '@vue/test-utils'
+import Sum from '@/components/Sum.vue'
+
+describe('FAILS Sum.vue', () => {
+  it('FAILS adds numbers from 2 inputs in real-time', () => {
+    const nb1 = 1
+    const nb2 = 1
+    const result = 3
+    const wrapper = shallowMount(Sum)
+    wrapper.vm.nb1 = nb1
+    wrapper.vm.nb2 = nb2
+    const actualResult = wrapper.find('.resultOfSum').text()
+    expect(parseFloat(actualResult)).toBe(result)
+  })
+})
